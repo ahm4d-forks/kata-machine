a5357980edbe0553d0aedd6bdc812f19bd801630
ahmad <aimt1@protonmail.com>
2023-06-30 17:11:20 +0300

    add linear search implementation

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,8 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for (const num of haystack) {
+        if (num === needle) {
+            return true;
+        }
+    }
+    return false;
+}
